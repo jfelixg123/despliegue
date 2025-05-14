906128836c0951dcfe6deaeae0a9633192a55b56
Jeancarlos Félix Guevara <jfelixg2324@politecnics.barcelona>
2025-05-14 17:23:22 +0200

    13
    13

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -8,7 +8,7 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-window.axios.defaults.baseURL = import.meta.env.VITE_APP_URL;
+window.axios.defaults.baseURL = 'api/';
 axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('auth_token')}`;
 
 
